d74a084761dee99c78cf87bb1ca370e24f0fbfd9
dipankarshaw <dipcom02323@gmail.com>
2022-05-17 13:44:26 +0530

    Code modified to handle AR/SR/AG, push code added.

--- Code.py
+++ Code.py
@@ -3,20 +3,20 @@ import os
 from jinja2 import Template
 
 file_path = os.path.dirname(os.path.realpath(__file__))
-wb = load_workbook(filename = file_path + '/input/PM_site_region.xlsx',read_only=True)
+wb = load_workbook(filename = file_path + '/input/1.xlsx',read_only=True)
 
 with open(file_path + '/input_template/telemetry.j2','r') as f:
     Temp = f.read()
 
 def Create_config(node):
     with open(file_path + f"/output/{node['node_name']}.txt", 'w+') as file_open:
-        config_command = Template(Temp).render(**node)                     
+        config_command = Template(Temp).render(**node)
         file_open.write(config_command)
         print(f"**** file created for {node['node_name']}")
 for row in wb.active.rows:
     node = {}
     node['node_name'] = row[0].value
     node['role'] = row[1].value
-    node['region'] = row[2].value
-    node['PM_IP'] = row[3].value
+    node['PM_IP'] = row[2].value
+    node['PM_Port'] = row[3].value
     Create_config(node)
---
+++ write_to_nodes.py
@@ -0,0 +1,26 @@
+from netmiko import Netmiko
+import os
+
+file_path = os.path.dirname(os.path.realpath(__file__))
+
+with open(file_path + '/input_template/node_ids.txt','r') as hostnames:
+    iq_device_list = hostnames.readlines()
+
+for device in iq_device_list:
+    try:
+        net_connect = Netmiko(
+            device_type="cisco_xr",
+            host=device,
+            username='dshaw1',
+            password='put_pass',
+            global_delay_factor=2,
+        )
+        with open(file_path + f'/output/{device}.txt','r') as f:
+            f2 = f.readlines()
+            output = net_connect.send_config_set(f2)
+            print(output)
+            net_connect.commit()
+            net_connect.exit_config_mode()
+        print(f"{device} Login:> success push:>success")
+    except:
+        print(f"{device} Login:> Failed push:> NA")
